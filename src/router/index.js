8f896e08ac179a6c0269f3a6f19a871da3c8e9db
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-05-12 15:50:34 +0100

    Add routes to the each page

--- src/router/index.js
+++ src/router/index.js
@@ -11,5 +11,51 @@ export default new Router({
       name: 'Home',
       component: Home,
     },
+    {
+      path: '/first',
+      name: 'First',
+      component: First,
+      meta: {
+        breadcrumb: [
+          { name: 'First page' }
+        ]
+      }
+    },
+    {
+      path: '/second',
+      name: 'Second',
+      component: Second,
+      meta: {
+        breadcrumb: [
+          { name: 'First page', link: 'first' },
+          { name: 'Second page' }
+        ]
+      }
+    },
+    {
+      path: '/third',
+      name: 'Third',
+      component: Third,
+      meta: {
+        breadcrumb: [
+          { name: 'First page', link: 'first' },
+          { name: 'Second page', link: 'second' },
+          { name: 'Third page' }
+        ]
+      }
+    },
+    {
+      path: '/fourth',
+      name: 'Fourth',
+      component: Fourth,
+      meta: {
+        breadcrumb: [
+          { name: 'First page', link: 'first' },
+          { name: 'Second page', link: 'second' },
+          { name: 'Third page', link: 'third' },
+          { name: 'Fourth page' }
+        ]
+      }
+    },
   ],
 });
