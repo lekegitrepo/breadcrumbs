60dd809196644d92ac2045ff84d178a16c117f14
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-05-25 13:55:59 +0100

    Update third child component

--- src/components/third-child.vue
+++ src/components/third-child.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <h1>Third Child page!</h1>
+    <router-link :to="{name: 'First'}"> Start Breadcrumbs</router-link>
+    <router-link :to="{name: 'Second'}"> Go to Second</router-link>
+    <router-link :to="{name: 'Fourth'}"> Go to Fourth</router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Third',
+}
 </script>
