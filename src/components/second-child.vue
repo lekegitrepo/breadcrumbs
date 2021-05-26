7c3860d8eee274aa32fd4bd28489deec0c6043d9
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-05-26 07:57:31 +0100

    Add route link to the third page

--- src/components/second-child.vue
+++ src/components/second-child.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h1>Second Child page!</h1>
+    <router-link :to="{name: 'First'}"> Start Breadcrumbs</router-link>
+    <router-link :to="{name: 'Third'}"> Go to Third</router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Second',
+}
 </script>
