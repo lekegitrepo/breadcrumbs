cbd72c70dfbe9b6bcca8bd50f00830b2e31db044
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-05-28 08:04:42 +0100

    Add links to other pages

--- src/components/fourth-child.vue
+++ src/components/fourth-child.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <h1>Fourth Child page!</h1>
+    <router-link :to="{name: 'First'}"> Start Breadcrumbs</router-link>
+    <router-link :to="{name: 'Second'}"> Go to Second</router-link>
+    <router-link :to="{name: 'Third'}"> Go to Third</router-link>
+    <router-link :to="{name: 'Fourth'}"> Go to Fourth</router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Fourth',
+}
 </script>
