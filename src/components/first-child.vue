13e461dddcb3df63af8a4fb242356508521705ab
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-05-31 07:24:26 +0100

    Add initial link

--- src/components/first-child.vue
+++ src/components/first-child.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <h1>First Child page!</h1>
+    <router-link :to="{name: 'Second'}"> Start Breadcrumbs</router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'First',
+}
 </script>
